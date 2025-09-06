1186ab5c4f3c56b9d901b3cf95589983ef256a4e
henstyle <henspicsaypro@gmail.com>
2025-09-07 05:24:01 +0700

    Create kontol.sh

---
+++ kontol.sh
@@ -0,0 +1,22 @@
+apt update
+apt install -y docker.io npm
+
+# MEMBUAT DAN INSTALL COLI
+
+git clone https://github.com/oneevil/stratum-ethproxy
+cd stratum-ethproxy
+npm install
+
+# SETUP GPU POINTING
+LOCAL_IP=$(hostname -I | awk '{print $1}')
+
+cat <<EOL >> .env
+REMOTE_HOST=asia.rplant.xyz
+REMOTE_PORT=7059
+REMOTE_PASSWORD=x
+LOCAL_HOST=$LOCAL_IP
+LOCAL_PORT=80
+EOL
+
+# MENJALANKAN STRATUM GULA
+screen -dmS GULA npm start
